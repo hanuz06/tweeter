c87cda696a1c4502e40d064408146274f09ed3eb
hanuz06 <lee1975uz@gmail.com>
2019-11-06 05:49:00 +0000

    added css and html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,102 @@
+
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// --- our code goes here ---
+
+
+const data = [{
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+const createTweetElement = function (tweetData) {
+  return (`
+      <article class='tweet'>
+        <header class="header">
+          <div class="header__user">
+            <div class="header__user__avatar">
+              <img src="${tweetData.user.avatars}">
+            </div>
+            <div class="header__user__name">
+              <h2>${tweetData.user.name}</h2>
+            </div>
+          </div>
+          <div class="header__user__id">
+           <span>${tweetData.user.handle}y</span> 
+          </div>
+        </header>
+  
+        <main class="user-tweeter">
+          <div class="user-tweeter__text">
+            <span>${tweetData.content.text}</span>      
+          </div>
+        </main>
+  
+        <footer class="footer">
+          <div class="time-ago">
+              <span>${tweetData.created_at}</span>
+          </div>
+          <div class="footer__links">
+            <div class="footer__links__icon">
+              <a href="#"><i class="fas fa-flag"></i></a>
+            </div>
+            <div class="footer__links__icon">
+              <a href="#"><i class="fas fa-retweet"></i></a>
+            </div>
+            <div class="footer__links__icon">
+              <a href="#"><i class="fas fa-thumbs-up"></i></a>
+            </div>
+          </div>
+        </footer> 
+      </article>      
+    `);
+}
+
+const renderTweets = function (tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+
+  tweets.forEach(tweet => {
+    $('#tweet-dynamic-container').append(createTweetElement(tweet));
+  });
+};
+
+
+$(document).ready(function () {
+  renderTweets(data);
+});
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  // --- our code goes here ---
+  $('#text').on('blur', function () {
+    let inputLength = $(this).val().length;
+    let charLeft = 140 - Number(inputLength);
+
+    charLeft >= 0 ? $(this).siblings().find("span").text(charLeft).css("color", "#545149") : $(this).siblings().find("span").text(charLeft).css("color", "red");
+    
+  })
+});
